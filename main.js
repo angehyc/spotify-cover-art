124267b86e61657adcb7efd3d5b2b89574f9588a
ange <angela.ching@immutable.com>
2023-05-23 17:35:53 +1000

    smat

--- main.js
+++ main.js
@@ -2,13 +2,5 @@ import { paint } from "./help/paint";
 import { submitLink } from "./help/submitLink";
 import { paintStartScreen } from "./screens/startScreen";
 
-const doNothing = () => {};
-const fakeEvent = {
-  preventDefault: doNothing,
-};
-submitLink(fakeEvent);
-
-// paintStartScreen();
-// paintCoverScreen();
-
-//function for displaying the album image from Spotify API
+paintStartScreen();
+//change
