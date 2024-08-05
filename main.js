2b942bbc4468e7c3d171b57c61a13e29903c6072
ange <angela.ching@immutable.com>
2024-08-05 14:25:55 +1000

    add posthog

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-import { paint } from "./help/paint";
-import { submitLink } from "./help/submitLink";
 import { paintStartScreen } from "./screens/startScreen";
+import posthog from "posthog-js";
+
+posthog.init(process.env.POSTHOG_KEY, {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+});
 
 paintStartScreen();
 //change
