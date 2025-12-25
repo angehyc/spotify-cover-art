380ed023df1edddd1b036f7a93ef50b7b418fb80
ange <99806698+angehyc@users.noreply.github.com>
2025-12-26 01:51:26 +1100

    Update api.js

--- config/api.js
+++ config/api.js
@@ -1,4 +1,4 @@
 export const DEV_URL = "http://localhost:3000";
-export const PROD_URL = "https://www.spotifycover.art";
+export const PROD_URL = "https://www.downloadcover.art";
 
 export const API_BASE_URL = import.meta.env.DEV ? DEV_URL : PROD_URL;
